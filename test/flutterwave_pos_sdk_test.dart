0d6039077e0a9ed5145e8bba0413922f2922b0e0
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2024-05-17 16:34:49 +0100

    first commit

--- test/flutterwave_pos_sdk_test.dart
+++ test/flutterwave_pos_sdk_test.dart
@@ -1,29 +1,29 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterwave_pos_sdk/flutterwave_pos_sdk.dart';
-import 'package:flutterwave_pos_sdk/flutterwave_pos_sdk_platform_interface.dart';
-import 'package:flutterwave_pos_sdk/flutterwave_pos_sdk_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutterwave_pos_sdk/flutterwave_pos_sdk.dart';
+// import 'package:flutterwave_pos_sdk/flutterwave_pos_sdk_platform_interface.dart';
+// import 'package:flutterwave_pos_sdk/flutterwave_pos_sdk_method_channel.dart';
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockFlutterwavePosSdkPlatform
-    with MockPlatformInterfaceMixin
-    implements FlutterwavePosSdkPlatform {
+// class MockFlutterwavePosSdkPlatform
+//     with MockPlatformInterfaceMixin
+//     implements FlutterwavePosSdkPlatform {
 
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+//   @override
+//   Future<String?> getPlatformVersion() => Future.value('42');
+// }
 
-void main() {
-  final FlutterwavePosSdkPlatform initialPlatform = FlutterwavePosSdkPlatform.instance;
+// void main() {
+//   final FlutterwavePosSdkPlatform initialPlatform = FlutterwavePosSdkPlatform.instance;
 
-  test('$MethodChannelFlutterwavePosSdk is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelFlutterwavePosSdk>());
-  });
+//   test('$MethodChannelFlutterwavePosSdk is the default instance', () {
+//     expect(initialPlatform, isInstanceOf<MethodChannelFlutterwavePosSdk>());
+//   });
 
-  test('getPlatformVersion', () async {
-    FlutterwavePosSdk flutterwavePosSdkPlugin = FlutterwavePosSdk();
-    MockFlutterwavePosSdkPlatform fakePlatform = MockFlutterwavePosSdkPlatform();
-    FlutterwavePosSdkPlatform.instance = fakePlatform;
+//   test('getPlatformVersion', () async {
+//     FlutterwavePosSdk flutterwavePosSdkPlugin = FlutterwavePosSdk();
+//     MockFlutterwavePosSdkPlatform fakePlatform = MockFlutterwavePosSdkPlatform();
+//     FlutterwavePosSdkPlatform.instance = fakePlatform;
 
-    expect(await flutterwavePosSdkPlugin.getPlatformVersion(), '42');
-  });
-}
+//     expect(await flutterwavePosSdkPlugin.getPlatformVersion(), '42');
+//   });
+// }
